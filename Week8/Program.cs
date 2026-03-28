94b2c04ceff50437ab174e7c9825007cc6d2a8fc
wa1d7 <ignat.chybuk@gmail.com>
2026-03-28 16:08:00 +0200

    task 2 stage 3 program cs realization

--- Week8/Program.cs
+++ Week8/Program.cs
@@ -1,9 +1,31 @@
-﻿
-class Program
-{
-    public static void Main()
+﻿namespace Week8;
+
+    class Program
     {
-        //branch check
-    }
+        public static void Main()
+        {
+ 
+            Point p1 = new Point { X = 1, Y = 2 };
+            Point p2 = p1;          
+ 
+            p2.X = 10;
+ 
+            p1.Print("p1");         
+            p2.Print("p2");         
+            
+            
+            PointRef r1 = new PointRef { X = 1, Y = 2 };
+            PointRef r2 = r1;    
+ 
+            r2.X = 10;
+ 
+            r1.Print("r1");      
+            r2.Print("r2");         
+ 
+            BoxingTester.Test(); 
+        }
     
-}
+    }
+
+
+  
