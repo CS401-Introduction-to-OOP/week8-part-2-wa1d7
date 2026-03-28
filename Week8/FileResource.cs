fdc8e43f5a8f96fbabac322fe06216c51d1347e3
wa1d7 <ignat.chybuk@gmail.com>
2026-03-28 15:41:41 +0200

    task 1 stage 1

---
+++ Week8/FileResource.cs
@@ -0,0 +1,42 @@
+﻿namespace Week8;
+    
+public class FileResource : Resource, IDisposable
+{
+    private bool _disposed = false;
+ 
+    public FileResource(string name) : base(name) { }
+ 
+    public override void Open()
+    {
+        if (IsOpen)
+        {
+            Console.WriteLine($"[File] '{Name}' already opened");
+            return;
+        }
+        IsOpen = true;
+        Console.WriteLine($"[File] opened '{Name}'");
+    }
+ 
+    public override void Close()
+    {
+        if (!IsOpen)
+        {
+            Console.WriteLine($"[File] '{Name}' already closed");
+            return;
+        }
+        IsOpen = false;
+        Console.WriteLine($"[File] closed '{Name}'");
+    }
+ 
+    public void Dispose()
+    {
+        if (_disposed) return;
+ 
+        if (IsOpen)
+            Close();
+ 
+        _disposed = true;
+        Console.WriteLine($"[File] '{Name}' disposed");
+        GC.SuppressFinalize(this);
+    }
+}
---
+++ Week8/NetworkResource.cs
@@ -0,0 +1,42 @@
+﻿namespace Week8;
+
+public class NetworkResource : Resource, IDisposable
+{
+    private bool _disposed = false;
+ 
+    public NetworkResource(string name) : base(name) { }
+ 
+    public override void Open()
+    {
+        if (IsOpen)
+        {
+            Console.WriteLine($"[Network] '{Name}'alredy connected");
+            return;
+        }
+        IsOpen = true;
+        Console.WriteLine($"[Network] connected to '{Name}'");
+    }
+ 
+    public override void Close()
+    {
+        if (!IsOpen)
+        {
+            Console.WriteLine($"[Network] '{Name}' already disconnected");
+            return;
+        }
+        IsOpen = false;
+        Console.WriteLine($"[Network] disconnected from '{Name}'");
+    }
+ 
+    public void Dispose()
+    {
+        if (_disposed) return;
+ 
+        if (IsOpen)
+            Close();
+ 
+        _disposed = true;
+        Console.WriteLine($"[Network] '{Name}' disposed");
+        GC.SuppressFinalize(this);
+    }
+}
