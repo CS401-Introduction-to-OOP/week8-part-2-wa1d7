52fd4157ca0ea2d1455783fedd283e84886919fc
wa1d7 <ignat.chybuk@gmail.com>
2026-03-28 16:18:05 +0200

    task 3 stage 3 program cs commit yappi

--- Week8/Program.cs
+++ Week8/Program.cs
@@ -1,9 +1,25 @@
-﻿
+﻿namespace Week8;
 class Program
 {
     public static void Main()
     {
-        //branch task 3 check
+        var container = new DataContainer<DataItem>();
+        for (int i = 0; i < 10000; i++)
+        {
+            var item = new DataItem
+            {
+                Id = $"item-{i}",
+                Payload = new byte[1024]
+            };
+            container.AddItem(item);
+        }
+ 
+        Console.WriteLine($"Total size before GC: {container.GetTotalSize()} bytes");
+ 
+        GC.Collect();
+ 
+        Console.WriteLine("GC.Collect() was called.");
+        Console.WriteLine($"Total size after GC: {container.GetTotalSize()} bytes");
     }
     
 }
