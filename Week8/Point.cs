9e8088f79f6713fa4e1b1c12004a99616994d003
wa1d7 <ignat.chybuk@gmail.com>
2026-03-28 15:58:02 +0200

    task 2 stage 1 point and pointRef

---
+++ Week8/Point.cs
@@ -0,0 +1,12 @@
+﻿namespace Week8;
+
+public struct Point
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    
+    public void Print(string label)
+    {
+        Console.WriteLine($" [{label}] point (struct) ->  X = {X} ; Y = {Y}");
+    }
+}
---
+++ Week8/PointRef.cs
@@ -0,0 +1,12 @@
+﻿namespace Week8;
+
+public class PointRef
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+ 
+    public void Print(string label)
+    {
+        Console.WriteLine($" [{label}] PointRef ->  X = {X} ; Y = {Y}");
+    }
+}
