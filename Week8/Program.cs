42a691872fa5bd4c131da2fbdf78f182bf24e11b
wa1d7 <ignat.chybuk@gmail.com>
2026-03-28 15:51:23 +0200

    task 1 stage 3 program cs realization

--- Week8/Program.cs
+++ Week8/Program.cs
@@ -1,9 +1,28 @@
-﻿
+﻿namespace Week8;
 class Program
 {
     public static void Main()
     {
+        var file = new FileResource("report.txt");
+        var network = new NetworkResource("api.company.local");
+        var manager = new ResourceManager<Resource>();
         
+        manager.Add(file);
+        manager.Add(network);
+        
+        manager.OpenAll();
+        
+        Console.WriteLine("using block: temporary connection");
+        using (var temporatyconnect = new NetworkResource("temporary"))
+        {
+            temporatyconnect.Open();
+            Console.WriteLine($"IsOpen = {temporatyconnect.IsOpen}");
+        }  
+        Console.WriteLine("  outside block connection cleaned");
+        
+        manager.CloseAll();
+ 
+        Console.WriteLine("done.");
     }
     
 }
