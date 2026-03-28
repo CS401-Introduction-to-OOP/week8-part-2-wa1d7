120929fd1a736b99efd994aa8c7768b2bd00424b
wa1d7 <ignat.chybuk@gmail.com>
2026-03-28 15:49:08 +0200

    task 1 stage 2 resource manager

---
+++ Week8/ResourceManager.cs
@@ -0,0 +1,26 @@
+﻿namespace Week8;
+
+public class ResourceManager<T> where T : Resource
+{
+    private readonly List<T> _resources = new();
+ 
+    public void Add(T resource)
+    {
+        _resources.Add(resource);
+        Console.WriteLine($"[Manager] added resource: '{resource.Name}'.");
+    }
+ 
+    public void OpenAll()
+    {
+        Console.WriteLine("[Manager] opening all resources");
+        foreach (var resource in _resources)
+            resource.Open();
+    }
+ 
+    public void CloseAll()
+    {
+        Console.WriteLine("[Manager] closing all resources");
+        foreach (var resource in _resources)
+            resource.Close();
+    }
+}
