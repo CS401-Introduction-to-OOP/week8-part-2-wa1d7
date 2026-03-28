e1d1ef295290cf5bc8f3433948a5e59997ca5925
wa1d7 <ignat.chybuk@gmail.com>
2026-03-28 16:05:14 +0200

    task 2 stage 2 boxing Tester

---
+++ Week8/BoxingTester.cs
@@ -0,0 +1,23 @@
+﻿namespace Week8;
+
+public static class BoxingTester
+{
+    public static void Test()
+    {
+ 
+        int a = 10;
+        Console.WriteLine($"a (int, stack)= {a}");
+ 
+        object b = a;                       
+        Console.WriteLine($"b (object, heap) = {b} boxed copy of a");
+ 
+        int c = (int)b;                     
+        Console.WriteLine($" c (int, stack)= {c}unboxed from b");
+ 
+        a = 99;
+        Console.WriteLine($"After a = 99:");
+        Console.WriteLine($"a = {a}(changed)");
+        Console.WriteLine($"b = {b}  (still 10)");
+        Console.WriteLine($"c = {c}  (still 10 like b)");
+    }
+}
